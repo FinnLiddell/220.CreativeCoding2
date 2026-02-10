c725b7894ba8d9530a84f08965ac0cc45570cbf7
Finn Liddell <147475283+FinnLiddell@users.noreply.github.com>
2026-02-09 22:33:39 -0700

    Update sketch.js

--- HW 4/js/sketch.js
+++ HW 4/js/sketch.js
@@ -1,42 +1,32 @@
-let stars = [];
-let night = true;
-
-let food1;
-let food2;
-let foodAI;
+let food1, food2, foodAI;
 let titleFont;
+
+let xPos = 0;
+let moveRight = true;
+
+// TIMER variable
+let lastMoveTime = 0;
+let moveInterval = 1000; // move every 1 second
 
 function preload() {
   // Images
   food1 = loadImage("images/pizza1.jpg");
-  food2 = loadImage("images/pizza2.jpg");
+  food2 = loadImage("images/pizza2.png");
   foodAI = loadImage("images/ai-pizza.png");
 
   // Custom font
-  titleFont = loadFont("assets/PlayfulTime-BLBB8.ttf");
+  titleFont = loadFont("assets/titleFont.ttf");
 }
 
 function setup() {
-  createCanvas(600, 600);
-
- 
-  for (let i = 0; i < 30; i++) {
-    stars.push({
-      x: random(width),
-      y: random(height / 2),
-      size: random(2, 5)
-    });
-  }
+  createCanvas(800, 600);
+  textFont(titleFont);
 }
 
 function draw() {
-  
-  if (night) {
-    background(30, 40, 70);
-  } else {
-    background(120, 170, 220);
-  }
+  background(240);
 
+  // TITLE (upper-left)
   fill(0);
   textSize(32);
   text("My Favorite Food: Pizza", 20, 40);
@@ -48,43 +38,22 @@ function draw() {
   // Static images
   image(food1, 50, 100, 200, 150);
   image(food2, 300, 100, 200, 150);
-  
-  fill(180);
-  rect(100, 80, 400, 300);
 
+  // TIMER-BASED movement
+  if (millis() - lastMoveTime > moveInterval) {
+    if (moveRight) {
+      xPos += 50;
+    } else {
+      xPos -= 50;
+    }
 
-  fill(50, 80, 130);
-  rect(110, 90, 180, 130);
-  rect(310, 90, 180, 130);
-  rect(110, 230, 180, 130);
-  rect(310, 230, 180, 130);
+    if (xPos > width - 200 || xPos < 0) {
+      moveRight = !moveRight;
+    }
 
-  fill(255);
-  for (let s of stars) {
-    ellipse(s.x, s.y, s.size);
-    s.y += random(-0.5, 0.5);
-    s.x += random(-0.5, 0.5);
+    lastMoveTime = millis();
   }
 
-  
-  fill(240, 240, 200);
-  ellipse(mouseX, mouseY, 50);
-
-
-  fill(120);
-  ellipse(300, 420, 160, 100);
-
- 
-  ellipse(300, 350, 100, 80);
-
-
-  triangle(260, 320, 280, 270, 300, 320);
-  triangle(340, 320, 320, 270, 300, 320);
-
-
-  rect(360, 430, 70, 15, 10);
-
-  
-  fill(160);
-  rect(90, 400, 420, 20);
+  // Moving AI image
+  image(foodAI, xPos, 350, 200, 150);
 }
