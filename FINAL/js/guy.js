39cbf3d861d8df7755fce44e7186f7f5a7cdf0f9
Finn Liddell <147475283+FinnLiddell@users.noreply.github.com>
2026-05-06 14:10:40 -0600

    Update guy.js

--- FINAL/js/guy.js
+++ FINAL/js/guy.js
@@ -36,33 +36,27 @@ class Guy {
     p.strokeWeight(2.5);
     p.noFill();
 
-    // tray
     p.stroke(COL.mid); p.strokeWeight(3);
     p.line(-22, 0, 22, 0);
     p.stroke(COL.ink); p.strokeWeight(2);
     p.line(-22, 0, -26, -5);
     p.line( 22, 0,  26, -5);
 
-    // torso
     p.stroke(COL.ink); p.strokeWeight(2.5);
     p.line(0, 0, 0, -22);
 
-    // legs
     const lw = Math.sin(this.step) * 6;
     p.line(0, 0, -8 + lw, 14);
     p.line(0, 0,  8 - lw, 14);
     p.line(-8 + lw, 14, -13 + lw, 18);
     p.line( 8 - lw, 14,  13 - lw, 18);
 
-    // arms
     p.line(0, -14, -22, -6);
     p.line(0, -14,  22, -6);
 
-    // head
     p.fill(COL.cream); p.stroke(COL.ink); p.strokeWeight(2);
     p.ellipse(0, -30, 18, 18);
 
-    // face
     p.noStroke(); p.fill(COL.ink);
     if (this.catching > 0) {
       p.ellipse(-3, -30, 3, 3);
@@ -76,7 +70,6 @@ class Guy {
       p.line(-3, -26, 3, -26);
     }
 
-    // cup in hand when catching
     if (this.catching > 10) {
       p.noStroke(); p.fill(COL.cup_good);
       p.rect(8, -8, 8, 6, 1);
